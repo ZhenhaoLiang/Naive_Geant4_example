a9a381ab8dafcbb7e22c6cd1890c4fd552dae9fb
zhenhaoliang <2941526315@qq.com>
2024-10-03 02:08:01 -0700

    gamma particle gun

---
+++ include/ParticleSource.hh
@@ -0,0 +1,14 @@
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+
+class G4Event;
+class G4ParticleGun;
+
+class BeginnerGeneratorAction : public G4VUserPrimaryGeneratorAction {
+public:
+	BeginnerGeneratorAction();
+	~BeginnerGeneratorAction();
+	virtual void GeneratePrimaries(G4Event*);
+private:
+	G4ParticleGun* particleGun;
+};
